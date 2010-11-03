93b7a4485ffe44f18ac637c9f653e9d68f4ab3c4
Julio Souza <mail@thejcs.com.br>
2010-11-03 01:08:30 -0200

    added basic functions to manage SteamID

--- scripts/jquery.steam.js
+++ scripts/jquery.steam.js
@@ -13,5 +13,26 @@
 
 // Nothing here yet
 (function($){
-  $.steam = {};
+  $.steam = (function(){
+    var pre = '7656119';
+    var pos = 7960265728;
+    
+    return {
+      validSteamID: function(steamID) {
+        return steamID.match(/^STEAM_[0-1]:[0-1]:[0-9]+$/i);
+      },
+      getSteamID: function(steamID64) {
+        // Crop the SteamID64 and subtract the base
+        var val = parseInt(steamID64.substr(pre.length)) - pos;
+        return 'STEAM_0:' + (val & 1) + ':' + (val >> 1);
+      },
+      getSteamID64: function(steamID) {
+        if(!$.steam.validSteamID(steamID)) {
+          return false;
+        }
+        var parts = steamID.split(':');      alert(parseInt(parts[1]));  
+        return pre + (parseInt(parts[1]) + (parseInt(parts[2]) << 1) + pos);
+      }
+    };
+  })();  
 })(jQuery);
